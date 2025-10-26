0fade8b23ba6638731a772bc9ef3ce4a9069a6f8
Avneesh-k <ak1555672@gmail.com>
2025-10-26 11:59:03 +0530

    add project

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -22,6 +22,15 @@ const projects = [
     id: 3,
     title: "",
     description:
+      "Blog App - A dynamic web application that allows users to read, create, and share blogs across various categories. Implemented features like user authentication, search functionality, and responsive design for an enhanced user experience. Built using React, Node.js, and MongoDB.",
+    image: "/projects/blog-app.png",
+    tags: ["MongoDB", "Express.js", "React.js","Node.js"],
+    githubUrl: "https://github.com/Avneesh-k/Blog-App",
+  },
+  {
+    id: 4,
+    title: "",
+    description:
       "",
     image: "/projects/download.png",
     tags: ["                         ","                "],
